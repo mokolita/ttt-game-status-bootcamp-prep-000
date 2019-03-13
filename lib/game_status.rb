8aa84a8914bf697719a736a4a2031bdf9ee1b538
Meg Galvez <meg@oakmaine.com>
2019-03-13 16:37:17 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -75,9 +75,9 @@ end
       position_1 = board[win_index_1]
       position_2 = board[win_index_2]
       position_3 = board[win_index_3]
-      if (position_1 == "X" && position_2 == "X" && position_3 == "X")      
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"      
         return "X"
-      elsif (position_1 == "O" && position_2 == "O" && position_3 == "O")
+      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
        return "O"
       else 
     end 
