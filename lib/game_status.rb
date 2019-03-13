321950e8cd92c67cb43f9e8b9f51d9ca61c3a124
Meg Galvez <meg@oakmaine.com>
2019-03-13 16:41:45 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -68,8 +68,8 @@ end
  end 
  
  
- def winner(array)
-  if won?(array)
+ def winner(board)
+  if won?(board)
     WIN_COMBINATIONS.each do |win_combination|
       position_1 = array[win_combination[0]]
       position_2 = array[win_combination[1]]
