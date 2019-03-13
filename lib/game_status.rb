7837edd512a53af3415ba1e53bafc28af4f97e95
Meg Galvez <meg@oakmaine.com>
2019-03-13 16:43:35 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -71,9 +71,9 @@ end
  def winner(board)
   if won?(board)
     WIN_COMBINATIONS.each do |win_combination|
-      position_1 = array[win_combination[0]]
-      position_2 = array[win_combination[1]]
-      position_3 = array[win_combination[2]]
+      position_1 = board[win_combination[0]]
+      position_2 = board[win_combination[1]]
+      position_3 = board[win_combination[2]]
       if (position_1 == "X" && position_2 == "X" && position_3 == "X")
         return "X"
       elsif (position_1 == "O" && position_2 == "O" && position_3 == "O")
